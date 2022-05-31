f5dae10cc95735f1890c6db74ff17743685afa16
Venkat2811 <venkat@tiyaro.ai>
2022-05-31 12:13:49 +0000

    bumped version

--- tiyaro_handler_test.py
+++ tiyaro_handler_test.py
@@ -6,7 +6,8 @@ import traceback
 from tiyaro.sdk.test_utils.util import (TEST_COMMAND_ARG_INPUT,
                                         TEST_COMMAND_ARG_OUTPUT,
                                         TEST_COMMAND_ARG_PRETRAINED,
-                                        get_input_by_class,
+                                        TEST_COMMAND_ARG_VERBOSE,
+                                        get_input_by_model_type,
                                         get_pretrained_file_path,
                                         validate_and_save_test_input,
                                         validate_and_save_test_output)
@@ -21,26 +22,35 @@ if __name__ == "__main__":
         my_parser.add_argument(TEST_COMMAND_ARG_INPUT, type=str, required=True)
         my_parser.add_argument(TEST_COMMAND_ARG_OUTPUT,
                                type=str, required=False)
+        my_parser.add_argument(TEST_COMMAND_ARG_VERBOSE,
+                               type=str, required=False)
         args = my_parser.parse_args()
 
-        file_path = get_pretrained_file_path(args.pretrained)
-        test_input = get_input_by_class(args.input)
+        is_verbose = args.verbose
+        if is_verbose == 'True':
+            is_verbose = True
+        else:
+            is_verbose = False
+
+        file_path = get_pretrained_file_path(args.pretrained, is_verbose)
+        test_input = get_input_by_model_type(args.input, is_verbose)
         output_file = args.output
         if output_file == "None":
             output_file = None
+        
 
         ob = TiyaroHandler()
         ob.setup_model(pretrained_file_path=file_path)
         ob.declare_schema()
         print(f'INIT - Done')
 
-        validate_and_save_test_input(ob, test_input)
+        validate_and_save_test_input(ob, test_input, is_verbose)
 
         print(f'INFERENCE - Started')
         test_output = ob.infer(test_input)
         print(f'INFERENCE - Done')
 
-        validate_and_save_test_output(ob, test_output)
+        validate_and_save_test_output(ob, test_output, is_verbose)
 
         print('OUTPUT STARTS - {}'.format('*'*50))
         print(json.dumps(test_output, indent=4, sort_keys=True))
